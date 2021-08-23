886d1846cec2cb039485e181ea951422e9aff874
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-08-23 20:07:21 +1200

    Invert getCommitMessages

--- src/github.ts
+++ src/github.ts
@@ -14,10 +14,11 @@ export class GitHubCommitProvider implements CommitProvider {
   ): Promise<string[]> {
     const octokit = github.getOctokit(options.token);
 
+    // Webook payloads: https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
     const commits = await octokit.rest.repos.listCommits({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      sha: github.context.payload.before
+      sha: github.context.payload.after
     });
 
     const commitMessages = commits.data.map(c => c.commit.message);
