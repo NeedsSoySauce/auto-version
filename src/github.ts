c7dcb150de604451f5602eb9006766a3576a5195
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-08-23 23:17:59 +1200

    Moar logging

--- src/github.ts
+++ src/github.ts
@@ -15,6 +15,10 @@ export class GitHubCommitProvider implements CommitProvider {
   ): Promise<string[]> {
     const octokit = github.getOctokit(options.token);
 
+    logger.info('-------- START CONTEXT --------');
+    logger.info(JSON.stringify(github.context, null, 2));
+    logger.info('-------- END CONTEXT ----------');
+
     // Webook payloads: https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
     const commit = await octokit.rest.repos.getCommit({
       owner: github.context.repo.owner,
@@ -26,13 +30,25 @@ export class GitHubCommitProvider implements CommitProvider {
     logger.info(JSON.stringify(commit, null, 2));
     logger.info('-------- END COMMIT ----------');
 
+    const commitData = commit.data.commit;
+    const since = commitData.author?.date || commitData.committer?.date;
+
+    if (!commit) {
+      throw new Error(
+        `No commit date found for commit ref '${github.context.ref}'`
+      );
+    }
+
     const commits = await octokit.rest.repos.listCommits({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      sha: github.context.payload.after
-      // since:
-      //   commit.data.commit.author?.date || commit.data.commit.committer?.date
+      sha: github.context.payload.after,
+      since
     });
+
+    logger.info('-------- START COMMITS --------');
+    logger.info(JSON.stringify(commits, null, 2));
+    logger.info('-------- END COMMITS ----------');
 
     const commitMessages = commits.data.map(c => c.commit.message);
 
