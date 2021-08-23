8fb4daa322bf1d48dba76387a7c59a8b71d51fe4
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-08-23 21:23:53 +1200

    Check commit payload

--- src/github.ts
+++ src/github.ts
@@ -1,4 +1,5 @@
 import * as github from '@actions/github';
+import { logger } from './logging';
 
 export interface GetCommitsOptions {
   token: string;
@@ -15,10 +16,22 @@ export class GitHubCommitProvider implements CommitProvider {
     const octokit = github.getOctokit(options.token);
 
     // Webook payloads: https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
+    const commit = await octokit.rest.repos.getCommit({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      ref: github.context.ref
+    });
+
+    logger.info('-------- START COMMIT --------');
+    logger.info(JSON.stringify(commit, null, 2));
+    logger.info('-------- END COMMIT ----------');
+
     const commits = await octokit.rest.repos.listCommits({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      sha: github.context.payload.after
+      sha: github.context.payload.after,
+      since:
+        commit.data.commit.author?.date || commit.data.commit.committer?.date
     });
 
     const commitMessages = commits.data.map(c => c.commit.message);
