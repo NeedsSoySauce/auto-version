f34dcfcce0324acc70dd7e63562fb407d623403d
NeedsSoySauce <feras.albaroudi@gmail.com>
2021-08-24 13:01:55 +1200

    Run version command

--- src/action.ts
+++ src/action.ts
@@ -1,7 +1,8 @@
-import { OutputProvider } from './output';
+import { getExecOutput } from '@actions/exec';
 import { GitHubCommitProvider } from './github';
-import { Logger, NullLogger } from './logging';
 import { InputProvider } from './input';
+import { Logger, NullLogger } from './logging';
+import { OutputProvider } from './output';
 
 export interface ActionOptions {
   input: InputProvider;
@@ -9,6 +10,8 @@ export interface ActionOptions {
   git: GitHubCommitProvider;
   logger?: Logger;
 }
+
+type Version = 'major' | 'minor' | 'patch';
 
 export class Action {
   private input: InputProvider;
@@ -23,6 +26,10 @@ export class Action {
     this.git = options.git;
   }
 
+  private hasItemWithPrefix(items: string[], prefixes: string[]): boolean {
+    return items.some(item => prefixes.some(prefix => item.startsWith(prefix)));
+  }
+
   public async run(): Promise<void> {
     const inputs = this.input.getInputs();
     this.logger.info(JSON.stringify(inputs, null, 2));
@@ -31,6 +38,22 @@ export class Action {
       token: inputs.token
     });
     this.logger.info(JSON.stringify(commitMessages, null, 2));
+
+    let version: Version;
+    if (this.hasItemWithPrefix(commitMessages, inputs.major)) {
+      version = 'major';
+    } else if (this.hasItemWithPrefix(commitMessages, inputs.minor)) {
+      version = 'minor';
+    } else if (this.hasItemWithPrefix(commitMessages, inputs.patch)) {
+      version = 'patch';
+    } else {
+      this.logger.info('No matching prefix was found, exiting.');
+      return;
+    }
+
+    const result = await getExecOutput(`npm version`, [version]);
+
+    this.logger.info(result.stdout);
 
     this.output.setOutputs({
       oldVersion: '0.1.0',
